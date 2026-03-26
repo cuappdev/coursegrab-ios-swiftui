563969f564eaed6d969c055bc5275c40502adc70
Jiwon Jeong <jiwonjeong414@gmail.com>
2026-03-26 14:10:38 -0400

    first commit

---
+++ CourseGrab/CourseGrabApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseGrabApp.swift
+//  CourseGrab
+//
+//  Created by jiwon jeong on 3/26/26.
+//
+
+import SwiftUI
+
+@main
+struct CourseGrabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CourseGrabTests/CourseGrabTests.swift
@@ -0,0 +1,17 @@
+//
+//  CourseGrabTests.swift
+//  CourseGrabTests
+//
+//  Created by jiwon jeong on 3/26/26.
+//
+
+import Testing
+@testable import CourseGrab
+
+struct CourseGrabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
